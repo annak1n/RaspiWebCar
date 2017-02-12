8c571e820979ef0cbb29db0bc0f454e5f51caa2c
neptune46 <neptune46@outlook.com>
2017-02-12 22:03:46 +0800

    receive button click in flask app

--- webcar.py
+++ webcar.py
@@ -1,9 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/control_car', methods=['POST'])
+def led_handler():
+    print("Button pressed!")
+    if request.form['ctrlbtn'] == "forward":
+        print("...forward...")
+    elif request.form['ctrlbtn'] == "backward":
+        print("...backward...")
+    elif request.form['ctrlbtn'] == "turnleft":
+        print("...turnleft...")
+    elif request.form['ctrlbtn'] == "turnright":
+        print("...turnright...")
+    else:
+        print("ERROR")
     return render_template('index.html')
 
 if __name__ == '__main__':
